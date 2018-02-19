911f21b7d4674d18a2ad17bece966e0c82adce27
Aurélien Perrier <a.perrier89@gmail.com>
2018-02-19 23:41:59 +0100

    Updating version package

--- version/version.go
+++ version/version.go
@@ -2,26 +2,18 @@ package version
 
 import (
 	"fmt"
-
-	version "github.com/hashicorp/go-version"
 )
 
-// The main version number that is being run at the moment.
+// Version number that is being run at the moment.
 const Version = "0.0.1"
 
-// A pre-release marker for the version. If this is "" (empty string)
+// Repository Git
+const Repository = "https://github.com/perriea/tfwrapper"
+
+// Prerelease marker for the version. If this is "" (empty string)
 // then it means that it is a final release. Otherwise, this is a pre-release
 // such as "dev" (in development), "beta", "rc1", etc.
 var Prerelease = "dev"
-
-// SemVer is an instance of version.Version. This has the secondary
-// benefit of verifying during tests and init time that our version is a
-// proper semantic version, which should always be the case.
-var SemVer = version.Must(version.NewVersion(Version))
-
-// Header is the header name used to send the current terraform version
-// in http requests.
-const Header = "Terraform-Version"
 
 // String returns the complete version string, including prerelease
 func String() string {
