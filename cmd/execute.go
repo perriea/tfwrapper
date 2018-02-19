3c492ddff11321575ee77f4a278e24857e2c3daa
Aurélien Perrier <a.perrier89@gmail.com>
2018-02-19 23:39:49 +0100

    Changing cli

---
+++ cmd/execute.go
@@ -0,0 +1,26 @@
+package cmd
+
+import (
+	"github.com/perriea/tfwrapper/pkg/wrapper"
+)
+
+// Run command
+func Run() {
+	// check len args
+	if len(args) > 0 {
+		// verify first arg
+		for cli, object := range c {
+			// if command exist
+			if cli == args[0] {
+				if object().Authenticated {
+					wrapper.ActionAuth(cli, args, object().Quiet)
+				} else {
+					wrapper.Action(cli, args)
+				}
+			}
+		}
+	} else {
+		// launch helper
+		wrapper.Help()
+	}
+}
