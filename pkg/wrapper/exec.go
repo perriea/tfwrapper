bfcdd1dd5fab6ddc09abc3e9d29e8b1287563491
Aurélien Perrier <a.perrier89@gmail.com>
2018-02-23 01:21:56 +0100

    Refactoring execution functions

--- pkg/wrapper/exec.go
+++ pkg/wrapper/exec.go
@@ -1,65 +1,32 @@
-/*
- * MIT License
- *
- * Copyright (c) 2017 Aurelien PERRIER
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the 'Software'), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
-
-/*
- * Revision History:
- *     Initial: 2017/12/08      Aurelien PERRIER
- */
-
 package wrapper
 
 import (
 	"os"
 	"os/exec"
 	"strings"
-	"time"
 )
 
 // func Lookup() {}
 
-// execution : Execute command
+// execution : Execute terraform command
 func execution(args []string) error {
 
-	// Prepare command with arguments
-	cmd := exec.Command("terraform", strings.Join(args, " "))
+	// err = terraform.Install("0.10.8")
+	// FatalError(err)
 
-	// redirect stdout && stdin
+	// Prepare command with arguments
+	cmd := exec.Command(binary, strings.Join(args, " "))
+
+	// redirect stdout/err/in
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	cmd.Stdin = os.Stdin
 
 	// Execute command
-	cmd.Run()
-
-	timer := time.AfterFunc(1*time.Second, func() {
-		err := cmd.Process.Kill()
-		FatalError(err)
-		os.Exit(1)
-	})
-
-	cmd.Wait()
-	timer.Stop()
+	err = cmd.Run()
+	if Error(err) {
+		return err
+	}
 
 	return nil
 }
