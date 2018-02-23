7b4ea1df9b216f1fe4398c64c78bb2e8562503b6
Aurélien Perrier <a.perrier89@gmail.com>
2018-02-23 01:22:41 +0100

    Adding tests

--- pkg/wrapper/exec_test.go
+++ pkg/wrapper/exec_test.go
@@ -4,7 +4,24 @@ import (
 	"testing"
 )
 
-// Exec : Execute command
-func Testexecution(t *testing.T) {
-	execution()
+// TestExecution : Execute command
+func TestExcution(t *testing.T) {
+	i := 0
+	tables := []struct {
+		args   []string
+		result bool
+	}{
+		{[]string{"apply"}, false},
+		{[]string{"^&*%&^"}, false},
+		{[]string{"fmt"}, true},
+		{[]string{""}, false},
+	}
+
+	for i < len(tables) {
+		err = execution(tables[i].args)
+		if err != nil && tables[i].result {
+			t.Fatal(err.Error())
+		}
+		i++
+	}
 }
