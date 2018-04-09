bd2200b7ea7eb771b35da694f60681d4457211a3
Aurélien Perrier <a.perrier89@gmail.com>
2018-04-09 22:09:01 +0200

    Updating GCP project example

--- examples/project/europe-west1/kubernetes/main.tf
+++ examples/project/europe-west1/kubernetes/main.tf
@@ -2,5 +2,9 @@ provider "google" {
   credentials = "${file(var.gcp_credentials)}"
   project     = "${var.gcp_project}"
   region      = "${var.gcp_region}"
-  version = "1.5"
+  version     = "1.8"
+}
+
+provider "random" {
+  version = "1.2"
 }
--- examples/project/europe-west1/kubernetes/outputs.tf
+++ examples/project/europe-west1/kubernetes/outputs.tf
@@ -1,7 +1,1 @@
-output "bucket_state_self_link" {
-  value = "${google_storage_bucket.image-store.self_link}"
-}
 
-output "bucket_state_url" {
-  value = "${google_storage_bucket.image-store.url}"
-}
